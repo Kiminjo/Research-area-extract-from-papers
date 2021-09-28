35fd25deb353eb4150ca04bae9ca573af3c0fde0
sy jin <rubby0922@gmail.com>
2021-09-29 04:46:09 +0900

    lda and h-clustering

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
-import argparse
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 29 01:13:17 2021
+
+@author: rubby
+"""
+
+import os
 import nltk
 import pickle
-import os
+import gensim
+import numpy as np
+import pandas as pd
+from gensim import corpora
+import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
-from gensim.models.doc2vec import Doc2Vec
-from sklearn.cluster import KMeans
-from sklearn.manifold import TSNE
-from sklearn.metrics.pairwise import cosine_similarity
+from gensim.matutils import cossim
 from nltk.stem import WordNetLemmatizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-from doc2vec import Doc_to_vec, TaggedDocument
-from argparse import ArgumentParser
-import warnings 
-warnings.filterwarnings('ignore')
+filepath = 'data/clean_data.pickle' # path of preprocessed data
 
-filepath = 'data/clean_data.pickle'
-lemma = WordNetLemmatizer()
-
-# 이 코드 쓰실 분은 여기만 수정하시면 됩니다.
 def text_preprocessing(data) :
+    lemma = WordNetLemmatizer()
     clean_data = []
     tokens = []
-    s_stopwords = load_stopwords()
     if (os.path.isfile(filepath)) :
         clean_data = load_clean()
     else :
@@ -36,13 +34,12 @@ def text_preprocessing(data) :
             for word in words :
                 if len(word) > 1:
                         if word not in stopwords.words('english') :
-                        ##and word not in s_stopwords
                             clean_words.append(lemma.lemmatize(word))                     
             clean_data.append(clean_words)
         print('Preprocessing complete')
         with open(filepath, 'wb') as lf :
             pickle.dump(clean_data, lf)
-    return [TaggedDocument(words=word, tags=[str(index)]) for index, word in enumerate(clean_data)]
+    return clean_data
 
 def load_clean() :
     with open(filepath, 'rb') as lf :
@@ -50,94 +47,29 @@ def load_clean() :
             
     return clean_data
 
-def load_stopwords() :
-    list_file = open('scientificstopwords.txt', 'r').read().split('\n')
-    return list_file
-
-if __name__ == '__main__':
-
-    parser = ArgumentParser(description='Doc2vec learning')
-    parser.add_argument('--data', type=str, help='text data path')
-    parser.add_argument('--parameter', type=str, help='hyperparameter path')
-    parser.add_argument('--mode', type=str, default='both', help='Doc2vec type. Just train or embedding and both')
-    parser.add_argument('--model', type=str, help='Call trained model')
-    parser.add_argument('--save_model_name', type=str, help='Save model name')
-    args = parser.parse_args()
-
-    mode = args.mode
-    params_dict = {'epochs' : None, 'embedding_size' : None, 'alpha' : None, 'min_alpha' : None, 'min_count' : None, 'dm' : None, 'window' : None}
-
-    with open(args.parameter, 'r') as f :
-        params = [[z[0].strip(), z[1].strip()] for z in [param.split(':') for param in f.readlines()]]
-        for param in params : 
-            params_dict[param[0]] = float(param[1])
-
-    epochs = params_dict['epochs'] 
-    embedding_size = params_dict['embedding_size']
-    alpha = params_dict['alpha']
-    min_alpha = params_dict['min_alpha']
-    min_count = params_dict['min_count']
-    dm = params_dict['dm']
-    window = params_dict['window']
-
-    ### Doc2vec abstarct embedding
-
-    df = pd.read_csv(args.data)
-    data = [row['Title'] + row['Abstract'] + str(row['Author Keywords']) + str(row['Index Keywords']) for _, row in df.iterrows()]
-    ##data = [row['Title'] + row['Abstract'] for idx, row in df.iterrows()]
-    processed_data = text_preprocessing(data)
+def calculate_similarity(tokenized_doc) :
+    dictionary = corpora.Dictionary(tokenized_doc)
+    dictionary.filter_extremes(no_below=10, no_above=0.5)
+    corpus = [dictionary.doc2bow(text) for text in tokenized_doc]
     
-    '''
-    clean_data = load_clean()
-    clean = []
+    model = gensim.models.ldamodel.LdaModel(corpus, 
+                                               num_topics = 40, 
+                                               id2word = dictionary, 
+                                               passes = 30, 
+                                               iterations = 400, 
+                                               random_state = 1004)
     
-    for doc in clean_data :
-        words = ''
-        for word in doc :
-            words += str(word) + ' '
-        clean.append(words)
-    '''
+    possibility_vector = pd.DataFrame(model[corpus])
     
-    if mode == 'both' :
-        model = Doc_to_vec(document=processed_data, epochs=epochs, embedding_size=embedding_size, alpha=alpha, min_count=min_count, min_alpha=min_alpha, window=window, model_name=args.save_model_name)
-        model.train()
-
-        tokenized_text = [nltk.word_tokenize(doc.lower()) for doc in data]
-        embedding_vectors = [model.doc2vec_model.infer_vector(text) for text in tokenized_text]
-
-    elif mode == 'train' :
-        model = Doc_to_vec(document=processed_data, epochs=epochs, embedding_size=embedding_size, alpha=alpha, min_count=min_count, min_alpha=min_alpha, window=window, model_name=args.save_model_name)
-        model.train()
-        exit()
-
-    elif mode == 'embedding' :
-        model = Doc2Vec.load(args.model)
-        tokenized_text = [nltk.word_tokenize(doc.lower()) for doc in data]
-        embedding_vectors = [model.infer_vector(text) for text in tokenized_text]
-
-    similarity_matrix = pd.DataFrame(cosine_similarity(embedding_vectors, embedding_vectors))
-    print(similarity_matrix)
+    for idx in range(len(possibility_vector.columns)) :
+        possibility_vector[idx] = possibility_vector[idx].apply(lambda x : x[1])
     
-    now = '210902_'
-    network_name = 'network/' + now + str(int(embedding_size)) + '_' + str(int(epochs)) + '.csv'
-    similarity_matrix.to_csv(network_name, index=True)
-
-
-    print('Embedding matrix size : {}'.format(np.array(embedding_vectors).shape))
-    print('Doc2vec embedding complete')
-
-    ### Clustering
-
-    kmeans = KMeans(n_clusters=30).fit(embedding_vectors)
-    clusters = kmeans.labels_
-
-    print('Clustering complete')
-
-
-    ### Visualization using T-SNE
-
-    two_dim_embedded_vectors = TSNE(n_components=2).fit_transform(embedding_vectors)
-
-    fig, ax = plt.subplots(figsize=(16,10))
-    sns.scatterplot(two_dim_embedded_vectors[:,0], two_dim_embedded_vectors[:,1], hue=clusters, palette='deep', ax=ax)
-    plt.show()
+    similarity_matrix = pd.DataFrame(cosine_similarity(possibility_vector, possibility_vector))
+    
+    return similarity_matrix
+    
+if __name__ == '__main__' :
+    raw_data = pd.read_csv('data/2016-2021.csv')
+    data = [row['Title'] + row['Abstract'] + str(row['Author Keywords']) + str(row['Index Keywords']) for _, row in raw_data.iterrows()]
+    clean_data = text_preprocessing(data)
+    similarity_matrix = calculate_similarity(clean_data)
---
+++ main_prev.py
@@ -0,0 +1,143 @@
+import argparse
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import nltk
+import pickle
+import os
+from nltk.corpus import stopwords
+from gensim.models.doc2vec import Doc2Vec
+from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
+from sklearn.metrics.pairwise import cosine_similarity
+from nltk.stem import WordNetLemmatizer
+
+from doc2vec import Doc_to_vec, TaggedDocument
+from argparse import ArgumentParser
+import warnings 
+warnings.filterwarnings('ignore')
+
+filepath = 'data/clean_data.pickle'
+lemma = WordNetLemmatizer()
+
+# 이 코드 쓰실 분은 여기만 수정하시면 됩니다.
+def text_preprocessing(data) :
+    clean_data = []
+    tokens = []
+    s_stopwords = load_stopwords()
+    if (os.path.isfile(filepath)) :
+        clean_data = load_clean()
+    else :
+        for doc in data :
+            tokens.append(nltk.regexp_tokenize(doc.lower(), '[A-Za-z]+'))                        
+        for words in tokens :
+            clean_words = []
+            for word in words :
+                if len(word) > 1:
+                        if word not in stopwords.words('english') :
+                        ##and word not in s_stopwords
+                            clean_words.append(lemma.lemmatize(word))                     
+            clean_data.append(clean_words)
+        print('Preprocessing complete')
+        with open(filepath, 'wb') as lf :
+            pickle.dump(clean_data, lf)
+    return [TaggedDocument(words=word, tags=[str(index)]) for index, word in enumerate(clean_data)]
+
+def load_clean() :
+    with open(filepath, 'rb') as lf :
+        clean_data = pickle.load(lf)
+            
+    return clean_data
+
+def load_stopwords() :
+    list_file = open('scientificstopwords.txt', 'r').read().split('\n')
+    return list_file
+
+if __name__ == '__main__':
+
+    parser = ArgumentParser(description='Doc2vec learning')
+    parser.add_argument('--data', type=str, help='text data path')
+    parser.add_argument('--parameter', type=str, help='hyperparameter path')
+    parser.add_argument('--mode', type=str, default='both', help='Doc2vec type. Just train or embedding and both')
+    parser.add_argument('--model', type=str, help='Call trained model')
+    parser.add_argument('--save_model_name', type=str, help='Save model name')
+    args = parser.parse_args()
+
+    mode = args.mode
+    params_dict = {'epochs' : None, 'embedding_size' : None, 'alpha' : None, 'min_alpha' : None, 'min_count' : None, 'dm' : None, 'window' : None}
+
+    with open(args.parameter, 'r') as f :
+        params = [[z[0].strip(), z[1].strip()] for z in [param.split(':') for param in f.readlines()]]
+        for param in params : 
+            params_dict[param[0]] = float(param[1])
+
+    epochs = params_dict['epochs'] 
+    embedding_size = params_dict['embedding_size']
+    alpha = params_dict['alpha']
+    min_alpha = params_dict['min_alpha']
+    min_count = params_dict['min_count']
+    dm = params_dict['dm']
+    window = params_dict['window']
+
+    ### Doc2vec abstarct embedding
+
+    df = pd.read_csv(args.data)
+    data = [row['Title'] + row['Abstract'] + str(row['Author Keywords']) + str(row['Index Keywords']) for _, row in df.iterrows()]
+    ##data = [row['Title'] + row['Abstract'] for idx, row in df.iterrows()]
+    processed_data = text_preprocessing(data)
+    
+    '''
+    clean_data = load_clean()
+    clean = []
+    
+    for doc in clean_data :
+        words = ''
+        for word in doc :
+            words += str(word) + ' '
+        clean.append(words)
+    '''
+    
+    if mode == 'both' :
+        model = Doc_to_vec(document=processed_data, epochs=epochs, embedding_size=embedding_size, alpha=alpha, min_count=min_count, min_alpha=min_alpha, window=window, model_name=args.save_model_name)
+        model.train()
+
+        tokenized_text = [nltk.word_tokenize(doc.lower()) for doc in data]
+        embedding_vectors = [model.doc2vec_model.infer_vector(text) for text in tokenized_text]
+
+    elif mode == 'train' :
+        model = Doc_to_vec(document=processed_data, epochs=epochs, embedding_size=embedding_size, alpha=alpha, min_count=min_count, min_alpha=min_alpha, window=window, model_name=args.save_model_name)
+        model.train()
+        exit()
+
+    elif mode == 'embedding' :
+        model = Doc2Vec.load(args.model)
+        tokenized_text = [nltk.word_tokenize(doc.lower()) for doc in data]
+        embedding_vectors = [model.infer_vector(text) for text in tokenized_text]
+
+    similarity_matrix = pd.DataFrame(cosine_similarity(embedding_vectors, embedding_vectors))
+    print(similarity_matrix)
+    
+    now = '210902_'
+    network_name = 'network/' + now + str(int(embedding_size)) + '_' + str(int(epochs)) + '.csv'
+    similarity_matrix.to_csv(network_name, index=True)
+
+
+    print('Embedding matrix size : {}'.format(np.array(embedding_vectors).shape))
+    print('Doc2vec embedding complete')
+
+    ### Clustering
+
+    kmeans = KMeans(n_clusters=30).fit(embedding_vectors)
+    clusters = kmeans.labels_
+
+    print('Clustering complete')
+
+
+    ### Visualization using T-SNE
+
+    two_dim_embedded_vectors = TSNE(n_components=2).fit_transform(embedding_vectors)
+
+    fig, ax = plt.subplots(figsize=(16,10))
+    sns.scatterplot(two_dim_embedded_vectors[:,0], two_dim_embedded_vectors[:,1], hue=clusters, palette='deep', ax=ax)
+    plt.show()
